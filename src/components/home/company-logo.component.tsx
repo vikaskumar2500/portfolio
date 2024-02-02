4843c99258275aaa359d84351811c617de9af56a
EugeneDraitsev <ddrrai@gmail.com>
2024-02-02 23:29:27 +0100

    Minor responsive css fixes

--- src/components/home/company-logo.component.tsx
+++ src/components/home/company-logo.component.tsx
@@ -9,7 +9,7 @@ export const CompanyLogo = ({ name, className }: CompanyLogoProps) => {
   return (
     <div className={`${className}`}>
       <Image
-        className="max-w-[60px] rounded-full object-contain"
+        className="-ml-4 mb-2 max-w-[60px] rounded-full object-contain md:m-0"
         width={120}
         height={120}
         alt="logo"
