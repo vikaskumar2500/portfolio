152580860c61aaca36c199cb1d0da77b50b7ec69
EugeneDraitsev <ddrrai@gmail.com>
2024-02-02 23:12:40 +0100

    CI fix

--- next.config.js
+++ next.config.js
@@ -1,13 +1,12 @@
-const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
 
 const withPWA = require('next-pwa')({
   dest: 'public',
-})
+});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: { optimizeCss: true },
-}
+};
 
-module.exports = PHASE_DEVELOPMENT_SERVER ? nextConfig : withPWA(nextConfig)
+module.exports = PHASE_DEVELOPMENT_SERVER ? nextConfig : withPWA(nextConfig);
