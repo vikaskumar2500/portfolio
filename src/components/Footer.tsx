ee5a02f920ee3563f6b94faf127b07b393c4120e
vikaskumar2500 <vikas.nits8084@gmail.com>
2024-04-30 09:24:50 +0530

    fixed footer section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="mb-10 px-4 text-center text-gray-500">
-      <small className="mb-2 block text-xs">&copy; 2030 Vikas, All rights reserved.</small>
+      <small className="mb-2 block text-xs">&copy; 2024 Vikas, All rights reserved.</small>
       <p className="text-xs">
         <span className="font-semibold">About this website:</span> build with React and Next.js(App Router
         & Server Actions), Typescript, Tailwind CSS, Varcel hosting.
